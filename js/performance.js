33f4e73c639552e9b9202caab3f1964fa67855f7
SofronieKevinDavid <davidsof01@gmail.com>
2019-01-28 19:47:43 +0200

    Performance looking good.

--- js/performance.js
+++ js/performance.js
@@ -2,22 +2,22 @@ var editId;
 
 // TODO edit API url's
 var API_URL = {
-    CREATE: './api/performance.json',
-    READ: './api/performance.json',
-    UPDATE: './api/update.json',
-    DELETE: './api/delete.json'
+    CREATE: '../api/performance.json',
+    READ: '../api/performance.json',
+    UPDATE: '../api/update.json',
+    DELETE: '../api/delete.json'
 };
 
 window.Eye = {
     getRow: function(history) {
         // ES6 string template
         return `<tr>
-            <td>${history.date}</td>
-            <td>${history.result}</td>
-            <td>${history.runnedGameLevel}</td>
-            <td>${history.gameName}</td>
+            <td style="width:10px">${history.date}</td>
+            <td style="width:100px">${history.result}</td>
+            <td style="width:100px">${history.runnedGameLevel}</td>
+            <td style="width:100px">${history.gameName}</td>
             <td>
-                <a href='../html/performance.html' data-id='${history.id}' class='delete'>&#128465;</a>
+                <a href='../html/performance.html' data-id='${history.id}' class='delete' style="color:whitesmoke">&#128465;</a>
             </td>
         </tr>`;
     },
@@ -32,15 +32,15 @@ window.Eye = {
         });
     },
 
-    getActionRow: function() {
-        // ES5 string concatenation
-        return '<tr>' +
-            '<td><input type="text" required name="firstName" placeholder="Enter first name"></td>' +
-            '<td><input type="text" name="lastName" placeholder="Enter last name"></td>' +
-            '<td><input type="text" required name="phone" placeholder="Enter phone"></td>' +
-            '<td><button type="submit">Save</button></td>' +
-            '</tr>';
-    },
+    // getActionRow: function() {
+    //     // ES5 string concatenation
+    //     return '<tr>' +
+    //         '<td><input type="text" required name="firstName" placeholder="Enter first name"></td>' +
+    //         '<td><input type="text" name="lastName" placeholder="Enter last name"></td>' +
+    //         '<td><input type="text" required name="phone" placeholder="Enter phone"></td>' +
+    //         '<td><button type="submit">Save</button></td>' +
+    //         '</tr>';
+    // },
 
     delete: function(id) {
         $.ajax({
@@ -82,15 +82,15 @@ window.Eye = {
     },
 
     bindEvents: function() {
-        $('#phone-book tbody').delegate('a.edit', 'click', function () {
+        $('#performance tbody').delegate('a.edit', 'click', function () {
             var id = $(this).data('id');
-            PhoneBook.edit(id);
+            Eye.edit(id);
         });
 
-        $('#phone-book tbody').delegate('a.delete', 'click', function () {
+        $('#performance tbody').delegate('a.delete', 'click', function () {
             var id = $(this).data('id');
             console.info('click on ', this, id);
-            PhoneBook.delete(id);
+            Eye.delete(id);
         });
 
         $( ".add-form" ).submit(function() {
@@ -102,9 +102,9 @@ window.Eye = {
 
             if (editId) {
                 person.id = editId;
-                PhoneBook.save(person);
+                Eye.save(person);
             } else {
-                PhoneBook.add(person);
+                Eye.add(person);
             }
         });
     },
@@ -140,7 +140,7 @@ window.Eye = {
 
         // ES6 function systax inside forEach
         histories.forEach(history => rows += Eye.getRow(history));
-        rows +=Eye.getActionRow();
+        // rows +=Eye.getActionRow();
         $('#performance tbody').html(rows);
     }
 };
