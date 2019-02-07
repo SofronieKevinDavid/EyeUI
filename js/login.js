84c8c9b4cb4f701e497676a658adc089f633bc57
SofronieKevinDavid <davidsof01@gmail.com>
2019-02-07 15:42:07 +0200

    update

--- js/login.js
+++ js/login.js
@@ -20,35 +20,6 @@ window.Eye = {
          }).error(function (xhr) {
              alert("wrong");
          });
-        /*$.ajax({
-            type: "GET",
-            url: "/cgi-bin/login.pl", // URL of the Perl script
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            // send username and password as parameters to the Perl script
-            data: "username=" + person.name + "&password=" + person.password,
-            // script call was *not* successful
-            error: function(XMLHttpRequest, textStatus, errorThrown) {
-                $('div#loginResult').text("responseText: " + XMLHttpRequest.responseText
-                    + ", textStatus: " + textStatus
-                    + ", errorThrown: " + errorThrown);
-                $('div#loginResult').addClass("error");
-            }, // error
-            // script call was successful
-            // data contains the JSON values returned by the Perl script
-            success: function(data){
-                if (data.error) { // script returned error
-                    $('div#loginResult').text("data.error: " + data.error);
-                    $('div#loginResult').addClass("error");
-                } // if
-                else { // login was successful
-                    $('form#loginForm').hide();
-                    $('div#loginResult').text("data.success: " + data.success
-                        + ", data.userid: " + data.userid);
-                    $('div#loginResult').addClass("success");
-                } //else
-            } // success
-        }); // ajax*/
     },
 
     bindEvents: function() {
