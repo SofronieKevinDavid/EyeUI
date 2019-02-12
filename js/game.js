ddc48943d331b8bf6a02debb7fde0260c5b23225
SofronieKevinDavid <davidsof01@gmail.com>
2019-02-12 21:12:56 +0200

    update la game

--- js/game.js
+++ js/game.js
@@ -1,53 +1,76 @@
-var canvas=document.getElementById("myCanvas");
-var ctx=canvas.getContext("2d");
+var result=0;
 
+function moveDiv() {
+    var $span = $("#random");
+    console.log("a");
 
-function make_base()
-{
-    base_img = new Image();
-    base_img.src = '../images/E.png';
-    return base_img;
+    $span.fadeOut(1000, function() {
+        var maxLeft = $(window).width() - $span.width();
+        var maxTop = $(window).height() - $span.height();
+        var leftPos = Math.floor(Math.random() * (maxLeft + 1))
+        var topPos = Math.floor(Math.random() * (maxTop + 1))
+
+        $span.css({ left: leftPos, top: topPos }).fadeIn(1000);
+    });
+};
+
+var arrows = [
+    {
+        "rotation":"rotate(-90deg)",
+        "key":"38"//up
+    },
+    {
+        "rotation":"rotate(-180deg)",
+        "key":"37"//left
+    },
+    {
+        "rotation":"rotate(-270deg)",
+        "key":"40"//down
+    },
+    {
+        "key":"38"//right
+    }
+];
+
+var a=[1,2,3,4];
+var rand=1;
+
+function rotateRandom(l){
+    x=arrows[l];
+    $("#random").style.webkitTransform=x.rotation;
 }
 
 
-init();
-function init()
-{
-    ctx.beginPath();
-    var img=make_base();
-    base_img.onload = function(){
-        ctx.drawImage(img, 0, 0,4, 4);
-    };
 
-    ctx.closePath();
+function portocala(){
+    rand = a[Math.floor(Math.random() * a.length)];
+    rotateRandom(rand);
+    return rand;
 }
 
-function makeNewPosition(img){
-    var h = $(window).height() - 50;
-    var w = $(window).width() - 50;
+function checkKey(e) {
+    
+    e = e || window.event;
 
-    var nh = Math.floor(Math.random() * h);
-    var nw = Math.floor(Math.random() * w);
+    if ((e.keyCode == '38')&(rand==1)) {
+        // up arrow
+        result++;
+    }
+    else if (e.keyCode == '37'&(rand==2)) {
+        // left arrow
+        result++;
+    }
+    else if (e.keyCode == '40'&(rand==3)) {
+        // down arrow
+        result++;
+    }
+    else if (e.keyCode == '39'&(rand==4)) {
+        // right arrow
+        result++;
+    }
 
-    return [nh,nw];
-
-    getNewAngle(Math.random()*90 ,Math.random()*90);
+    moveDiv();
+    portocala();
 }
 
-function getNewAngle(prev, next){
-    var x = prev[1] - next[1];
-    var y = prev[0] - next[0];
-    var ang = Math.atan(Math.abs(y)/Math.abs(x))/(Math.PI/180)
-    if(x>0&&y>0)
-        return ang;
-    else if(x<0&&y>0)
-        return ang+90;
-    else if(x>0&&y<0)
-        return ang-90;
-    else
-        return ang+180
-}
-
-img.onkeypress =function(){
-    makeNewPosition(img);
-}
+document.onkeydown = checkKey;
